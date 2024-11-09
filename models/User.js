2112a9dcaa08acc171176c929d26e2bdce943e26
putrafauzan32@gmail.com <email@example.com>
2024-11-09 10:26:26 +0700

    update api

--- models/User.js
+++ models/User.js
@@ -1,30 +1,26 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type : String, 
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    password : {
-        type : String, 
-        required : true, 
-        unique:true
+    password: {
+        type: String,
+        required: true,
+        unique: true
     },
-    email : {
-        type : String, 
-        required : true},
-    profilePicture : {
+    email: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
         type: Buffer,
-        default : null
+        default: null
     },
-    createdAt : {
-        type : String,
-        required : true
-    },
-    updatedAt : {
-        type : String,
-        required : true
-    },
-})
+}, {
+    timestamps: true 
+});
+
 
 export default mongoose.model("User", userSchema);
